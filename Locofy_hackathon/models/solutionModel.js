373a9813afdddceda0fb03f7f38a85e8a53e072b
R4d104ct1v1ty <101217134+R4d104ct1v1ty@users.noreply.github.com>
2023-01-05 18:34:54 +0530

    Added the backend

---
+++ Locofy_hackathon/models/solutionModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const solutionSchema = new Schema({
+    body: {type: String, required: true},
+    userID: {type: Number, required: true},
+    date: {type: Date, default: Date.now},
+    roomID: {type: Number, required: true},
+    doubtID: {type: Number, required: true}
+});
+
+
+module.exports = mongoose.model('Solution', solutionSchema);
